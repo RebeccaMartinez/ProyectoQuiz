4e94b8276cbb9b0114d586df022bbcbed5a23ec6
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-12-14 18:05:18 +0000

    commit

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,12 +16,13 @@ exports.answer = function(req, res){
 	})
 };
 
-/*var Quiz = require('../models/quiz_model');
+var Quiz = require('../models/quiz_model');
 var quiz = new Quiz();
 var current = quiz.randomQuestion();
 exports.index = function(req, res, next) {
 	res.render('index', { title: 'Quiz' });
 };
+
 exports.question = function(req,res) {
 	current = quiz.randomQuestion();
 	res.render('quizes/question', {pregunta: current.pregunta });
@@ -54,4 +55,4 @@ exports.specificQuestion = function(req, res) {
 		current = quiz.q[id-1];
 		res.render('quizes/question', {pregunta: current.pregunta});
 	}
-};*/
+};
