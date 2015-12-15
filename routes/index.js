0fad5bc606c26a10e2188a257ef3ca9aebbe2432
QUINOESCOBAR <quinoescobar@gmail.com>
2015-12-15 14:23:25 +0000

    puesto en index.js lo que se habia puesto en app.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
-
+var pg = require('pg');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
@@ -14,5 +14,17 @@ router.get('/quizes/answer', quizController.answer);
 router.get('/quizes/questions/:id', quizController.specificQuestion);
 
 router.get('/quizes/questions', quizController.questions);
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM Quizzes', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 module.exports = router;
